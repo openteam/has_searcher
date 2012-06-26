eab6256b327a8b2d4f75e15fc7cac625b43497ba
Dmitry Lihachev <lda@openteam.ru>
2012-06-26 09:28:47 +0700

    allow to override default recip parameters

--- lib/app/models/boostificator.rb
+++ lib/app/models/boostificator.rb
@@ -1,43 +1,30 @@
 class Boostificator
   MILLIS_IN_YEAR = 1000 * 60 * 60 * 24 * 365
+  PRECISION = 6
 
   attr_accessor :search, :field
 
+  attr_accessor :m, :x, :a, :b
+
+  attr_accessor :now
+
   def initialize(options)
-    self.search = options[:search]
-    self.field = options[:field]
-  end
-
-  def m
-    1.0 / MILLIS_IN_YEAR
-  end
-
-  def a
-    1.1
-  end
-
-  def b
-    1
-  end
-
-  def precision
-    6
+    options.reverse_merge!(:a => 1.0, :b => 1.0, :m => 1.0 / MILLIS_IN_YEAR, :now => 1.hour.since.change(:min => 0))
+    options.each do |field, value|
+      self.send("#{field}=", value)
+    end
   end
 
   def recip_min
-    sprintf("%f", (recip * 10**precision).floor/10.0**precision)
+    sprintf("%f", (recip * 10**PRECISION).floor/10.0**PRECISION)
   end
 
   def recip_max
-    sprintf("%f", (recip * 10**precision).ceil/10.0**precision)
+    sprintf("%f", (recip * 10**PRECISION).ceil/10.0**PRECISION)
   end
 
   def recip
-    a / (m*now_ms + b)
-  end
-
-  def now
-    1.hour.since.change(:min => 0)
+    a / (m * now_ms + b)
   end
 
   def now_ms
@@ -50,7 +37,7 @@ class Boostificator
 
   def adjust_solr_params
     search.adjust_solr_params do |params|
-      params[:q] = "{!boost b=map(recip(ms(#{now_s},#{field}),#{m},#{a},#{b}),#{recip_min},#{recip_max},1) defType=dismax}#{params[:q]}"
+      params[:q] = "{!boost b=map(recip(ms(#{now_s},#{field}),#{m},#{a},#{b}),#{recip_min},#{recip_max},1) defType=dismax}#{params[:q]}" if params[:q]
     end
   end
 end
