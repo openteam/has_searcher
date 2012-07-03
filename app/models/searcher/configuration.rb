cbbd0cac4573f9a201bf3e5d9045b77146bdbdc1
Dmitry Lihachev <lda@openteam.ru>
2012-07-03 08:31:48 +0700

    rf: generate scopes in the suitable methods

--- app/models/searcher/configuration.rb
+++ app/models/searcher/configuration.rb
@@ -1,7 +1,7 @@
 class Searcher::Configuration
   attr_accessor :searcher, :scopes
 
-  attr_accessor :keywords_field, :properties, :facets
+  attr_accessor :properties, :facets
 
   attr_accessor :search_object_methods
 
@@ -12,31 +12,30 @@ class Searcher::Configuration
     self.facets = {}
     self.search_object_methods = Module.new
     instance_eval &block if block
-    scope do |sunspot|
-      sunspot.fulltext searcher.params[searcher.configuration.keywords_field]
-      searcher.configuration.properties.each do |property, options|
-        if modificator = options[:modificator]
-          request_field = "#{property}_#{modificator}".to_sym
-          sunspot.with(property).send(modificator, searcher.search_object.send(request_field)) if searcher.search_object.send(request_field).presence
-        end
-      end
-    end
   end
 
   def keywords(field)
-    self.keywords_field = field
-    property field
+    add_method_to_search_object(field)
+    scope do |sunspot|
+      sunspot.fulltext searcher.search_object.send(field) if searcher.search_object.send(field).presence
+    end
   end
 
   def property(field, options={})
     self.properties[field] = options
-    field = [field, options[:modificator]].compact.join('_')
-    search_object_methods.class_eval do
-      define_method "#{field}" do
-        instance_variable_get("@#{field}")
-      end
-      define_method "#{field}=" do |value|
-        instance_variable_set("@#{field}", value)
+
+    modificator = options[:modificator]
+    request_field = [field, modificator].compact.join('_')
+
+    add_method_to_search_object(request_field)
+
+    scope do |sunspot|
+      if searcher.search_object.send(request_field).presence
+        if modificator
+          sunspot.with(field).send(modificator, searcher.search_object.send(request_field))
+        else
+          sunspot.with(field, searcher.search_object.send(request_field))
+        end
       end
     end
   end
@@ -50,4 +49,16 @@ class Searcher::Configuration
     self.scopes[name] << block
   end
 
+  private
+
+    def add_method_to_search_object(request_field)
+      search_object_methods.class_eval do
+        define_method "#{request_field}" do
+          instance_variable_get("@#{request_field}")
+        end
+        define_method "#{request_field}=" do |value|
+          instance_variable_set("@#{request_field}", value)
+        end
+      end
+    end
 end
