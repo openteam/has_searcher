09b5b0bc3f15d5337c54c8d69b151ad11bcf54a7
Dmitry Lihachev <lda@openteam.ru>
2012-10-11 11:17:00 +0700

    make boostificator workable

--- app/models/searcher/boostificator.rb
+++ app/models/searcher/boostificator.rb
@@ -6,50 +6,56 @@ class Searcher::Boostificator
 
   attr_accessor :m, :x, :a, :b
 
-  attr_accessor :extra_boost
+  attr_accessor :now
+
+  attr_accessor :boosts
 
   def initialize(boost_field, options={})
     self.boost_field = boost_field
-    options.reverse_merge!(:a => 1.0, :b => 1.0, :m => 1.0 / MILLIS_IN_YEAR, :extra_boost => 1.0)
+    options.reverse_merge!(:a => 1.0, :b => 1.0, :m => 1.0 / MILLIS_IN_YEAR)
     options.each do |attribute, value|
       self.send("#{attribute}=", value)
     end
   end
 
-
-  def adjust_solr_params(sunspot)
-    sunspot.adjust_solr_params do |params|
-      params[:boost] = "product(#{extra_boost},#{recip_s})"
-      params[:defType] = 'edismax'
+  def adjust_solr_params(searcher)
+    searcher.configuration.scope do |sunspot|
+      sunspot.adjust_solr_params do |params|
+        params[:boost] = boosts + [recip_s]
+        params[:defType] = 'edismax'
+      end
     end
   end
 
-  private
-    def recip_s
-      @recip_s ||= "map(recip(abs(ms(#{now_s},#{boost_field})),#{m},#{a},#{b}),#{recip_min},#{recip_max},1)"
-    end
+  def boosts
+    @boosts ||= []
+  end
 
-    def now
-      @now ||= HasSearcher.cacheable_now
-    end
+  def recip_s
+    @recip_s ||= "map(recip(abs(ms(#{now_s},#{boost_field})),#{m},#{a},#{b}),#{recip_min},#{recip_max},1)"
+  end
 
-    def recip
-      @recip ||= a / (m * now_ms + b)
-    end
+  def now
+    @now ||= HasSearcher.cacheable_now
+  end
 
-    def now_ms
-      @now_ms ||= now.to_i * 1000
-    end
+  def recip
+    @recip ||= a / (m * now_ms + b)
+  end
 
-    def now_s
-      @now_s ||= now.utc.to_datetime.to_time.iso8601
-    end
+  def now_ms
+    @now_ms ||= now.to_i * 1000
+  end
 
-    def recip_min
-      @recip_min ||= sprintf("%f", (recip * 10**PRECISION).floor/10.0**PRECISION)
-    end
+  def now_s
+    @now_s ||= now.utc.to_datetime.to_time.iso8601
+  end
 
-    def recip_max
-      @recip_max ||= sprintf("%f", (recip * 10**PRECISION).ceil/10.0**PRECISION)
-    end
+  def recip_min
+    @recip_min ||= sprintf("%f", (recip * 10**PRECISION).floor/10.0**PRECISION)
+  end
+
+  def recip_max
+    @recip_max ||= sprintf("%f", (recip * 10**PRECISION).ceil/10.0**PRECISION)
+  end
 end
